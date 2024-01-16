b0a40dea802c9baca1680a5bd05efb13e6e13463
Rishi Ishairzay <windowasher@gmail.com>
2024-01-16 01:50:53 -0500

    Update title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta property='og:title' content='Title' />
 
-	<title>This is a title</title>
+	<title>Hey there, I'm Rishi Ishairzay</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,wght@0,300;0,500;1,400&family=Work+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">    
