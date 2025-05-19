0e35d91aee8c398129ee561bbcc904233263b1bb
Rishi Ishairzay <windowasher@gmail.com>
2025-05-19 00:55:24 -0400

    padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -194,7 +194,7 @@
 						class="shadow hover rounded-lg overflow-hidden transition-all duration-200 bg-white/40 border-8 border-white/40"
 					>
 						<img src={project.image} alt={project.title} class="w-full h-48 object-cover" />
-						<div class="p-6">
+						<div class="p-4">
 							<h3 class="text-xl font-bold mb-2">{project.title}</h3>
 							<p class="text-gray-600 mb-2">{project.description}</p>
 
